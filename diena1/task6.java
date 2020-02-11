74b9a6292152846b4e9b20d9557ee68c177206e5
Helmutux <59434588+Helmutux@users.noreply.github.com>
2020-02-11 17:15:57 +0200

    Add files via upload

--- diena1/task6.java
+++ diena1/task6.java
@@ -1,22 +1,23 @@
 
-//Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers
-import java.util.Scanner;
+//sudetis, atimtis, daugyba, dalyba, likutis
 
 class task6{
 	public static void main(String[] args) {
-  		Scanner in = new Scanner(System.in);
-   
+  		
+   		Integer first;
+   		Integer second;
+
 	  	System.out.print("Ivesti pirma skaiciu: ");
-	  	int numFirst = in.nextInt();
+	  	first = Integer.valueOf(System.console().readLine());
 	   
 	  	System.out.print("Ivesti antra skaiciu: ");
-	  	int numSecond = in.nextInt();
+	  	second = Integer.valueOf(System.console().readLine());
 	   
-	  	System.out.println(numFirst + " + " + numSecond + " = " + (numFirst + numSecond));
-	  	System.out.println(numFirst + " - " + numSecond + " = " + (numFirst - numSecond));
-	  	System.out.println(numFirst + " x " + numSecond + " = " + (numFirst * numSecond));
-	  	System.out.println(numFirst + " / " + numSecond + " = " + (numFirst / numSecond));
-	  	System.out.println(numFirst + " mod " + numSecond + " = " + (numFirst % numSecond));
+	  	System.out.println(first + " + " + second + " = " + (first + second));
+	  	System.out.println(first + " - " + second + " = " + (first - second));
+	  	System.out.println(first + " x " + second + " = " + (first * second));
+	  	System.out.println(first + " / " + second + " = " + (first / second));
+	  	System.out.println(first + " mod " + second + " = " + (first % second));
  	
 	}
 }
