5cc3d62f6b7456d9c06400337a26769c2edb7888
Helmutux <59434588+Helmutux@users.noreply.github.com>
2020-02-11 22:01:48 +0200

    Delete task8.java

--- diena1/task8.java
+++
@@ -1,7 +0,0 @@
-
-//Java program to display the following pattern
-class task8{
-	public static void main(String[] args) {
-   		System.out.println("   J    a   v     v  a  \nJ  J  aaaaa   V V  aaaaa \n JJ  a     a   V  a     a");
-	}
-}
