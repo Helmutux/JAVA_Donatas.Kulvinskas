13d9480d310bae5f9f75c94defca5aa8efc9b5de
Helmutux <59434588+Helmutux@users.noreply.github.com>
2020-02-17 13:31:23 +0200

    Pakoreguota 2 uzduotis

--- 3-pamoka/Task2.java
+++ 3-pamoka/Task2.java
@@ -4,13 +4,42 @@
 //     koordinates (eilutes indeksa, stulpelio indeksa).
 class Task2 {
     public static void main(String[] args) {
-        int matrix[][] = { { 1, 2, 3, 4, 5 }, { 10, 20, 30, 40, 50 }, { 100, 200, 300, 400, 500 } };
+        int matrix[][]= {
+        	{ 1, 2, 3, 4, 5 },
+        	{ 10, 20, 30, 40, 50 },
+        	{ 100, 200, 300, 400, 500 }
+        };
         int sumColumns = 0;
-        // int sumRows;
-        for (int i = 0; i < matrix.length; i++) {
-            sumColumns = matrix[i][i]+matrix[i+1][i]+matrix[i+2][i];
-            System.out.println(matrix[i+1] + " stulpelio suma yra " + sumColumns);    
-        }
-        
+        int sumRows = 0;
+        int numberSpecial = 300;
+        //aprasau cikla cikle, noredamas skaiciuoti masyvo stulpeliu sumas
+		for (int i= 0; i< matrix[0].length; i++) {
+			for (int j=0; j<matrix.length; j++) {
+				sumColumns += matrix[j][i];
+			}
+			//isvedu i ekrana masyvo stulpeliu suma
+            System.out.println((i+1) + " stulpelio suma:  " + sumColumns);
+            //nunulinu gauta reiksme, kad suma skaiciuotusi is naujo
+            sumColumns = 0;
+    	}
+    	//aprasau cikla, noredamas skaiciuoti masyvo eiluciu suma
+    	for(int i=0; i<matrix.length; i++){
+    		for(int j=0; j<matrix[0].length; j++){
+    			sumRows += matrix[i][j];
+    		}
+    		//isvedu i ekrana masyvo eiluciu suma
+            System.out.println((i+1) + " eilutes suma:  " + sumRows);
+            //nunulinu gauta reiksme, kad suma skaiciuotusi is naujo
+            sumRows = 0;
+    	}
+    	//ieskosiu pasirinkto skaiciaus (numberSpecial) koordinaciu masyve
+    	for(int i=0; i<matrix.length; i++){
+    		for(int j=0; j<matrix[i].length; j++){
+    			if(matrix[i][j]==numberSpecial){
+    				System.out.println("Pasirinkto skaiciaus " + numberSpecial + " vieta yra: " + (i+1) + " eiluteje ir " + (j+1) + " stulpelyje.");
+    			}
+    		}
+
+    	}
     }
 }
